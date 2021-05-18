853b24ef6edd92727c8738792f04f7bb3a1b3078
Karol <kmaksymiuk5555@gmail.com>
2021-05-18 13:52:03 +0200

    end

--- app/src/main/java/pollub/ism/lab09/AktywnoscGlowna.java
+++ app/src/main/java/pollub/ism/lab09/AktywnoscGlowna.java
@@ -1,6 +1,8 @@
 package pollub.ism.lab09;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,12 @@ public class AktywnoscGlowna extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aktywnosc_glowna);
+
+        DostawcaDanych dostawca = new DostawcaDanych(getResources());
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        NaszAdapter adapter = new NaszAdapter(this, dostawca);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
